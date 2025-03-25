6e30cccf0f3adb2da8e5d451946a62f4d576d1c9
Vladimir Abramichev <vladimir.abramichev@adyen.com>
2025-03-25 17:32:55 +0100

    Add event listener

--- android/app/src/main/java/com/testrn74/my_module/AlertDialogActivity.kt
+++ android/app/src/main/java/com/testrn74/my_module/AlertDialogActivity.kt
@@ -1,9 +1,13 @@
 package com.testrn74.my_module
 import android.app.Activity
 import android.content.Intent
+import android.icu.lang.UCharacter.VerticalOrientation
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.ProgressBar
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.AppCompatButton
 
 class AlertDialogActivity: Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,10 +19,27 @@ class AlertDialogActivity: Activity() {
         val builder = AlertDialog.Builder(this)
         val message = intent.getStringExtra("message")
         val title = intent.getStringExtra("title")
+        val view = LinearLayout(this)
+        view.orientation = LinearLayout.VERTICAL
+
+        val button1 = AppCompatButton(this)
+        button1.setText("Send event")
+        button1.setOnClickListener {
+            ForcedAlertModule.sendEvent(ForcedAlertModule.EVENT_A, message ?: "no message")
+        }
+
+
+        val button2 = AppCompatButton(this)
+        button2.setText("Send event with async")
+        button2.setOnClickListener {
+            ForcedAlertModule.sendEvent(ForcedAlertModule.EVENT_B, message ?: "no message")
+        }
+        view.addView(button1)
+        view.addView(button2)
+
         builder
-            .setView(ProgressBar(this))
+            .setView(view)
             .setTitle(title)
-            .setMessage(message)
             .setPositiveButton("Dismiss") { _, _ ->
                 finish()
             }
